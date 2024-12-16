e02890118771ddd02e366ca8273849de3b09f1da
marqasa <slwalkerpenn@gmail.com>
2024-12-16 20:27:53 +0000

    Add Widget schema

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Widget, Widgets } from './schema/Widget'
+import axios from 'axios'
+
+const widgets = ref<Widget[]>([])
+
+/**
+ * Fetch widgets from the API
+ */
+async function fetchWidgets() {
+  const response = await axios.get(
+    'https://b795b019-1f84-41f4-93a3-a702d686c75a.mock.pstmn.io/product-widgets',
+  )
+
+  // Check if the response data is valid according to the Widgets schema
+  if (Widgets.safeParse(response.data).success) {
+    widgets.value = response.data
+  } else {
+    console.error('Failed to parse widgets')
+  }
+}
+
+fetchWidgets()
+</script>
+
 <template>
   <main>
     <h1>Widgets</h1>
+    <pre>{{ widgets }}</pre>
   </main>
 </template>
---
+++ src/schema/Widget.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+
+export const WidgetType = z.enum(['carbon', 'plastic', 'trees'])
+
+export const WidgetAction = z.enum(['collects', 'plants', 'offsets'])
+
+export const WidgetColor = z.enum(['white', 'black', 'blue', 'green', 'beige'])
+
+export const Widget = z.object({
+  action: WidgetAction,
+  active: z.boolean(),
+  amount: z.number(),
+  id: z.number(),
+  linked: z.boolean(),
+  selectedColor: WidgetColor,
+  type: WidgetType,
+})
+
+export const Widgets = z.array(Widget)
+
+export type WidgetType = z.infer<typeof Widget>
+
+export type WidgetAction = z.infer<typeof WidgetAction>
+
+export type WidgetColor = z.infer<typeof WidgetColor>
+
+export type Widget = z.infer<typeof Widget>
+
+export type Widgets = z.infer<typeof Widgets>
